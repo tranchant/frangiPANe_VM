31fd34f23e1da601a080df677edae60d21c2b059
Christine Tranchant-Dubreuil <christine.tranchant@ird.fr>
2022-07-05 11:59:17 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script is executed on the virtual machine during the Installation phase (need to be ran as root!).
+# It is used to record a predefined VM-image of the appliance.
+# Otherwise executed first during a cloud deployement in IFB-Biosphere
+
